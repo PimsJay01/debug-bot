0c32b81f4112ec618ce54945300366ffa6bbdca6
Jérémy Gobet <jgobet@octo.com>
2017-07-01 11:26:20 +0200

    Add isometric map with player's starting position, holes & robots

--- front/src/res.js
+++ front/src/res.js
@@ -7,6 +7,14 @@ export const res = {
         readyBtn : images + 'mushroom2.png'
     },
     tiles : {
-        default : tiles + '001.png'
+        default : tiles + '001.png',
+        start1 : tiles + '011.png',
+        start2 : tiles + '012.png',
+        start3 : tiles + '013.png',
+        start4 : tiles + '014.png',
+        robot1n : tiles + 'robot_1_north.png',
+        robot1e : tiles + 'robot_1_east.png',
+        robot1s : tiles + 'robot_1_south.png',
+        robot1w : tiles + 'robot_1_west.png'
     }
 }
