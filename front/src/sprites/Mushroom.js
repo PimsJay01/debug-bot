ea2c5a728047a350a3057fd05ffb026de52f7fbc
Pierre Kuenzli <pierre.kuenzli@unige.ch>
2017-08-28 21:09:07 +0200

    added agpl license

--- front/src/sprites/Mushroom.js
+++ front/src/sprites/Mushroom.js
@@ -1,3 +1,23 @@
+/*
+Copyright 2017 Arnaud Beguin, Amelie Fiocca, Loic Poisot, Jeremy Gobet and Pierre Kunzli
+
+This file is part of Debug Bot.
+
+Debug Bot is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Debug Bot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
